3e273e9d18b4943a75afa1ed9f5b938378c177ca
pierrot281969-coder <pierrot.281969@gmail.com>
2026-03-19 17:42:39 +0900

    完成
    ハンバーガー作成
    そのた細かい修正

--- cafe_practice.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll",function(){
-    const header=this.document.querySelector(".header");
-    if(this.window.scrollY>50){
-        header.style.boxShadow="0 2px 8px rgba(0,0,0,0.1)";
-    }else{
-        header.style.boxShadow="none";
-    }
-});
